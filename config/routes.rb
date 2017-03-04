3e17470e1f8c186a4e0c5af6da145dcd7a3f3eb9
ismail <ismaeel700@gmail.com>
2017-03-04 18:47:19 +0000

    dsfd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
+
   devise_for :users, :controllers => { ragistrations: 'ragistrations' }
+    resources :articles
   root to: 'pages#index'
   get 'pages/contact'
   get 'pages/about'
